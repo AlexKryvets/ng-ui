50276f3f8154770345d0f74a146c8707b6c43d7c
Oleksii Kryvets <alex.kryvets@gmail.com>
2023-02-22 15:43:30 +0200

    ng generate lib resizable -p ng

---
+++ projects/resizable/src/lib/resizable.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-resizable',
+  template: `
+    <p>
+      resizable works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ResizableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/resizable/src/lib/resizable.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ResizableComponent } from './resizable.component';
+
+
+
+@NgModule({
+  declarations: [
+    ResizableComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ResizableComponent
+  ]
+})
+export class ResizableModule { }
